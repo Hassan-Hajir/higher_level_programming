765235b6cd58f4f51961e12795d25a2986fdebe2
root <hashajir@gmail.com>
2023-04-09 01:41:56 -0700

    fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,55 +1,31 @@
-#!/usr/bin/python3
+def width(self):
+     """Initializes a new Rectangle object.
 
-class Rectangle:
-    """Represents a rectangle.
-
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-    """
-
-    def __init__(self, width=0, height=0):
-        """Initializes a new Rectangle instance.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        self._width = width
-        self._height = height
-
-    @property
-    def width(self):
-        """Get the width of the rectangle."""
-        return self._width
+    Args:
+        width (int): The width of the new rectangle.
+        height (int): The height of the new rectangle.
+    """    
+    return self.__width
 
     @width.setter
+    """Returns the value of the width attribute."""
     def width(self, value):
-        """Set the width of the rectangle.
-
-        Args:
-            value (int): The new width of the rectangle.
-        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        """Get the height of the rectangle."""
-        return self._height
+        """Returns the value of the width attribute."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle.
-
-        Args:
-            value (int): The new height of the rectangle.
-        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
+
